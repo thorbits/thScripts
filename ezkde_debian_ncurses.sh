8bc4c6c223211cae76a4bc3a082b9fbefc796d1c
thorbits <36136807+thorbits@users.noreply.github.com>
2025-12-15 14:42:41 -0500

    ezkde_debian_ncurses.sh

--- ezkde_debian_ncurses.sh
+++ ezkde_debian_ncurses.sh
@@ -84,7 +84,8 @@ main() {
 
     # Show introduction dialog unless in silent mode
     if [[ "$silent" == false ]]; then
-        intro_text="This script will install a minimal KDE Plasma Wayland setup. Press OK to continue."
+        intro_text="This script will install a minimal KDE Plasma Wayland setup.
+        Press OK to continue."
         local centered_intro=$(center_text "$intro_text")
         whiptail --title "eZkde for Debian" --msgbox "$centered_intro" 12 78 || true
     fi
