f4df4585b34484460bf9ef7b41adc60b75b27120
thorbits <36136807+thorbits@users.noreply.github.com>
2025-12-22 14:17:44 -0500

    progress_bar_test.sh

--- progress_bar_test.sh
+++ progress_bar_test.sh
@@ -22,67 +22,66 @@ BAR_CHAR='|'
 EMPTY_CHAR=' '
 
 fatal() {
-    printf '[FATAL] %s\n' "$*" >&2
-    exit 1
+	echo '[FATAL]' "$@" >&2
+	exit 1
 }
 
 progress-bar() {
-    local current=$1
-    local len=$2
+	local current=$1
+	local len=$2
 
-    local perc_done=$((current * 100 / len))
+	local perc_done=$((current * 100 / len))
 
-    # Modified to show only percentage
-    local suffix=" ($perc_done%)"
+	local suffix=" ($perc_done%)"
 
-    local length=$((COLUMNS - ${#suffix} - 2))
-    local num_bars=$((perc_done * length / 100))
+	local length=$((COLUMNS - ${#suffix} - 2))
+	local num_bars=$((perc_done * length / 100))
 
-    local i
-    local s='['
-    for ((i = 0; i < num_bars; i++)); do
-        s+=$BAR_CHAR
-    done
-    for ((i = num_bars; i < length; i++)); do
-        s+=$EMPTY_CHAR
-    done
-    s+=']'
-    s+=$suffix
+	local i
+	local s='['
+	for ((i = 0; i < num_bars; i++)); do
+		s+=$BAR_CHAR
+	done
+	for ((i = num_bars; i < length; i++)); do
+		s+=$EMPTY_CHAR
+	done
+	s+=']'
+	s+=$suffix
 
-    printf '\e7' # save the cursor location
-      printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
-      printf '\e[0K' # clear the line
-      printf '%s' "$s" # print the progress bar
-    printf '\e8' # restore the cursor location
+	printf '\e7' # save the cursor location
+	  printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
+	  printf '\e[0K' # clear the line
+	  printf '%s' "$s" # print the progress bar
+	printf '\e8' # restore the cursor location
 }
 
 install-packages() {
     local packages=("$@")
 
-    printf "Installing set of %d KDE (meta-)packages\n" "${#packages[@]}"
+    printf "Installing set of %d KDE (meta-)packages\n\n" "${#packages[@]}"
 
     local pkg
     for pkg in "${packages[@]}"; do
-        printf "-> Now downloading and installing: %s\n" "$pkg"
+        echo -e "-> Now downloading and installing: ${#pkg[@]} \n\n"
         apt-get install -y "$pkg" >/dev/null 2>&1
     done
     sleep .1
 }
 
 init-term() {
-    printf '\n' # ensure we have space for the scrollbar
-      printf '\e7' # save the cursor location
-        printf '\e[%d;%dr' 0 "$((LINES - 1))" # set the scrollable region (margin)
-      printf '\e8' # restore the cursor location
-    printf '\e[1A' # move cursor up
+	printf '\n' # ensure we have space for the scrollbar
+	  printf '\e7' # save the cursor location
+	    printf '\e[%d;%dr' 0 "$((LINES - 1))" # set the scrollable region (margin)
+	  printf '\e8' # restore the cursor location
+	printf '\e[1A' # move cursor up
 }
 
 deinit-term() {
-    printf '\e7' # save the cursor location
-      printf '\e[%d;%dr' 0 "$LINES" # reset the scrollable region (margin)
-      printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
-      printf '\e[0K' # clear the line
-    printf '\e8' # reset the cursor location
+	printf '\e7' # save the cursor location
+	  printf '\e[%d;%dr' 0 "$LINES" # reset the scrollable region (margin)
+	  printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
+	  printf '\e[0K' # clear the line
+	printf '\e8' # reset the cursor location
 }
 
 main() {
