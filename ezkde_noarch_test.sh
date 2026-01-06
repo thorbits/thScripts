3f76e9e74d57ed6750843bdd3ac5033021751940
thorbits <36136807+thorbits@users.noreply.github.com>
2026-01-06 10:40:09 -0500

    ezkde_noarch_test.sh

--- ezkde_noarch_test.sh
+++ ezkde_noarch_test.sh
@@ -161,13 +161,13 @@ ART
         ;;
 esac
 
-printf ' ##############################################'
-printf '\n\n # Welcome %s, to eZkde for %s. #\n\n' "$USER" "$DISTRO"
-printf ' # __________________________________________ #\n'
+printf ' #----------------------------------------------------#\n'
+printf ' # Welcome %s, to eZkde for %s. #\n' "$USER" "$DISTRO"
+printf ' #----------------------------------------------------#\n'
 printf ' # The latest version of KDE 6.5.x (Wayland session) #\n will be installed with audio support (Pipewire) #\n and a minimum of utilities. #\n\n'
-printf ' # __________________________________________ #'
+printf ' #----------------------------------------------------#\n'
 printf ' # Press Enter to continue or Ctrl+C to cancel. #\n'
-printf ' ##############################################'
+printf ' #----------------------------------------------------#\n'
 read -rp '' && eval "$UPDATE" || fatal " ERROR: no internet connection detected. Exiting."
 
 progress-bar() {
