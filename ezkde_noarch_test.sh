632c30c9745ec97b3fa8dd641aad6788bbce4022
thorbits <36136807+thorbits@users.noreply.github.com>
2026-01-07 21:32:08 -0500

    ezkde_noarch_test.sh

--- ezkde_noarch_test.sh
+++ ezkde_noarch_test.sh
@@ -164,7 +164,6 @@ printf ' #---------------------------------------------------#\n'
 printf ' # The latest version of KDE 6.5.x (Wayland session) #\n # will be installed with audio support (Pipewire)   #\n # and a minimum of utilities.                       #\n'
 printf ' #---------------------------------------------------#\n\n'
 while true; do
-    printf '\r\033[2K'
     read -n1 -s -r -p ' Press Enter to continue or Ctrl+C to cancel.'
     # check if User pressed Ctrl+C
     if (( $? != 0 )); then
