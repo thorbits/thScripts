636c03a4c7827a76877bb30d78b2df5eab21b951
thorbits <36136807+thorbits@users.noreply.github.com>
2026-01-10 13:14:05 -0500

    ezkde_noarch.sh

--- ezkde_noarch.sh
+++ ezkde_noarch.sh
@@ -327,7 +327,7 @@ install_packages() {
     
     for pkg in "$@"; do
         printf '\r%-*s' "$COLUMNS" " -> Now downloading and installing: $pkg"
-        "${PM[@]}" "$pkg" >/dev/null
+        "${PM[@]}" "$pkg" 2>/dev/null
     done
 }
 
