2e101116235a70d2dda75187fd472cad82b69325
thorbits <36136807+thorbits@users.noreply.github.com>
2026-01-26 07:23:49 -0500

    ezkernel_debian.sh

--- ezkernel_debian.sh
+++ ezkernel_debian.sh
@@ -329,12 +329,9 @@ if [[ "$KCFG" == false ]]; then
         fatal "error generating kernel config."
     fi
 else
-	if [[ ! -f "config" || ! -r "config" || ! -s "config" ]]; then
+	if [[ ! -f ".config" || ! -r ".config" || ! -s ".config" ]]; then
         fatal "error: 'config' file is missing, unreadable, or empty."
     fi
-#	if ! mv -f config .config; then
-#        fatal "error copying configuration file to .config."
-#    fi
 fi
 case "$DISTRO" in
 	arch)
@@ -396,3 +393,4 @@ reboot_system(){
 }
 
 reboot_system
+
