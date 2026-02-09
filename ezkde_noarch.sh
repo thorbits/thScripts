fb5e15980ebe56715481d8d1639aa9eeb8245a0a
thorbits <36136807+thorbits@users.noreply.github.com>
2026-02-09 10:13:44 -0500

    ezkde_noarch.sh

--- ezkde_noarch.sh
+++ ezkde_noarch.sh
@@ -485,7 +485,7 @@ main() {
         opensuse)
             mapfile -t packages < <(
 				"${LIST_CMD[@]}" "${pkg_names[@]}" 2>&1 |
-				awk '/new/ {for(i=1;i<=NF;i++) if ($i ~ /^[a-zA-Z0-9.-]+$/) print $i}' | grep -v "session-x11" | grep -v "kwin6-x11" | head -n -5 
+				awk '/new/ {for(i=1;i<=NF;i++) if ($i ~ /^[a-zA-Z0-9.-]+$/) print $i}' | grep -v "session-x11" | grep -v "kwin6-x11" | grep -v "patterns-base-x11" | head -n -5 
 			)
 #				zypper in -y -D patterns-kde-kde_plasma konsole dolphin pipewire sddm | head -n -18 | tail -n +9 | xargs -n 1
 #				zypper se --requires kde konsole dolphin pipewire sddm | sed '/pattern$/d'
